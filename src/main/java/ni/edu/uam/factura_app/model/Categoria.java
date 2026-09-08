a0414a87756f3bd196c6abcffff14f84662837cc
Rommel110207 <rommelmg@uamv.edu.ni>
2026-09-08 16:01:45 -0600

    feat: Clases.

--- src/main/java/ni/edu/uam/factura_app/model/Categoria.java
+++ src/main/java/ni/edu/uam/factura_app/model/Categoria.java
@@ -1,6 +1,23 @@
 package ni.edu.uam.factura_app.model;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Categoria
 {
+    private Integer id;
+
+    private String nombre;
+
+    private boolean activa;
+
+    @Override
+
+    public String toString() {
+        return nombre;
+    }
+
 
 }
