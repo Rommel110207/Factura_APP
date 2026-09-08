55fcea947ad845b0305908185fb46a0b964e705e
Rommel110207 <rommelmg@uamv.edu.ni>
2026-09-08 16:48:14 -0600

    feat: Agregar vistas.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,6 @@ module ni.edu.uam.factura_app {
     requires static lombok;
 
 
-    opens ni.edu.uam.factura_app to javafx.fxml;
+    opens ni.edu.uam.factura_app.controller to javafx.fxml;
     exports ni.edu.uam.factura_app;
 }
