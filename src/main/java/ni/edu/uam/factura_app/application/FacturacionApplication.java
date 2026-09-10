c931cee469a8141646b5fde0e58155f58da0555c
Rommel110207 <rommelmg@uamv.edu.ni>
2026-09-10 15:48:51 -0600

    feat: Agregar la escena principal de la Aplicacion.

--- src/main/java/ni/edu/uam/factura_app/application/FacturacionApplication.java
+++ src/main/java/ni/edu/uam/factura_app/application/FacturacionApplication.java
@@ -1,4 +1,20 @@
 package ni.edu.uam.factura_app.application;
 
-public class FacturacionApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FacturacionApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ni/edu/uam/fxml/menu-principal.fxml"));
+        stage.setTitle("Sistema de facturación");
+        stage.setScene(new Scene(loader.load(), 900, 600));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
